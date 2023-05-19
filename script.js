8a7d0fd5fe0af546f904845c2c3981e02c2496d5
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-19 14:41:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	let arr=["0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"];
+	let ans=[];
+	for(let i=0;i<arr[input_digit[0]].length;i++){
+		for(let j=0;j<input_digit.length;j++){
+			
+		}
+	}
 }
 
 module.exports = letterCombinations;
