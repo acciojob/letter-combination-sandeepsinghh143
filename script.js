0950eeb85aae31bd7d16d71926ab5b0311ee4212
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-24 10:58:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 function letterCombinations(input_digit) {
   //Complete the function
-	let arr=["0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"];
-	let ans=[];
-	for(let i=0;i<arr[input_digit[0]].length;i++){
-		for(let j=0;j<input_digit.length;j++){
-			
-		}
-	}
+	if (digits.length === 0) {
+    return [];
+  }
+
+  const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  const result = [];
+
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+      return;
+    }
+
+    const currentDigit = nextDigits[0];
+    const letters = digitToLetters[currentDigit];
+
+    for (const letter of letters) {
+      backtrack(combination + letter, nextDigits.slice(1));
+    }
+  }
+
+  backtrack('', digits);
+
+  return result.sort();
 }
 
 module.exports = letterCombinations;
